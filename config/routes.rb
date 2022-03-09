965cca65ed5b14bbbe3d54f95ffad21e5156c460
William Meyer <williamjmiii@gmail.com>
2022-03-09 13:42:00 -0500

    recreated all products with alternate syntax, created /product path

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/products", controller:"products", action:"all_products"
+  get "/product", controller:"products", action:"first_products"
+  get "/product" => "products#all_products"
 end
