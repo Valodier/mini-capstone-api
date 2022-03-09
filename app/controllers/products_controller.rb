bc54d818462a84bb6bdf9b18e5d5ddd29b5da8ec
William Meyer <williamjmiii@gmail.com>
2022-03-09 13:32:02 -0500

    created route for all_products action, created all_products and first_product actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,9 @@
 class ProductsController < ApplicationController
+  def all_products
+    render json: Product.all.as_json
+  end
+
+  def first_product
+    render json: Product.first.as_json
+  end
 end
