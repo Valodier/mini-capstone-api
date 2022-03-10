eaeba83aa71e20d87664b82fabe369aea0beffa7
William Meyer <williamjmiii@gmail.com>
2022-03-10 14:50:34 -0500

    commented out seeded products in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-product1 = Product.new ({name: "LazerDizk", price:5, image_url:"", description:"An object used by our forefathers to watch media"})
-product1.save
 
-product2 = Product.new ({name: "Cannon", price:2000000, image_url:"", description:"A bear arm we have a right to"})
-product2.save
+# product1 = Product.new ({name: "LazerDizk", price:5, image_url:"", description:"An object used by our forefathers to watch media"})
+# product1.save
 
-product3 = Product.new ({name: "Three tires", price:20000, image_url:"", description:"Enough tires to force you to buy more"})
-product3.save
+# product2 = Product.new ({name: "Cannon", price:2000000, image_url:"", description:"A bear arm we have a right to"})
+# product2.save
 
-product4 = Product.new ({name: "Monopoly", price:60, image_url:"", description:"An educational game about how the rich always win"})
-product4.save
+# product3 = Product.new ({name: "Three tires", price:20000, image_url:"", description:"Enough tires to force you to buy more"})
+# product3.save
+
+# product4 = Product.new ({name: "Monopoly", price:60, image_url:"", description:"An educational game about how the rich always win"})
+# product4.save
