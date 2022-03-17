1e47b4bf010bffc3cf0a851edabeec38946ddd23
William Meyer <williamjmiii@gmail.com>
2022-03-17 14:05:51 -0400

    dumped db into seeds file with all fields generated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-# product1 = Product.new ({name: "LazerDizk", price:5, image_url:"", description:"An object used by our forefathers to watch media"})
-# product1.save
-
-# product2 = Product.new ({name: "Cannon", price:2000000, image_url:"", description:"A bear arm we have a right to"})
-# product2.save
-
-# product3 = Product.new ({name: "Three tires", price:20000, image_url:"", description:"Enough tires to force you to buy more"})
-# product3.save
-
-# product4 = Product.new ({name: "Monopoly", price:60, image_url:"", description:"An educational game about how the rich always win"})
-# product4.save
-
-supplier1 = Supplier.new ({name: "Facebook", email: "TheZuck@aol", phone_number: "111-1111"})
-supplier1.save
-supplier2 = Supplier.new ({name: "Waste Management", email: "Garbage@trash", phone_number: "333-3333"})
-supplier2.save
-supplier3 = Supplier.new ({name: "America", email: "Oligarchy@Rich", phone_number: "000-0000"})
-supplier3.save
+Supplier.create!([
+  {name: "Facebook", email: "TheZuck@aol", phone_number: "111-1111"},
+  {name: "Waste Management", email: "Garbage@trash", phone_number: "333-3333"},
+  {name: "America", email: "Oligarchy@Rich", phone_number: "000-0000"}
+])
+Product.create!([
+  {name: "LazerDizk", price: "5.0", image_url: "https://upload.wikimedia.org/wikipedia/commons/a/ab/LDDVDComparison-mod.png", description: "An object used by our forefathers to watch media", supplier_id: 2},
+  {name: "Torch", price: "14.0", image_url: "frankestiens_monsters_fears.jpeg", description: "Something frankenstien's monster is very afraid of", supplier_id: 2},
+  {name: "Torch", price: "14.0", image_url: "frankestiens_monsters_fears.jpeg", description: "Something frankenstien's monster is very afraid of", supplier_id: 2},
+  {name: "Three tires", price: "20000.0", image_url: "https://media.istockphoto.com/photos/old-dirty-tires-picture-id158360075?s=612x612", description: "Enough tires to force you to buy more", supplier_id: 3},
+  {name: "Cannon", price: "2000000.0", image_url: "https://m.media-amazon.com/images/I/71VUUu51KAL._AC_SY450_.jpg", description: "A bear arm we have a right to", supplier_id: 3},
+  {name: "Torch", price: "14.0", image_url: "frankestiens_monsters_fears.jpeg", description: "Something frankenstien's monster is very afraid of", supplier_id: 3},
+  {name: "Tarch", price: "14.0", image_url: "frankestiens_monsters_fears.jpeg", description: "Something frankenstien's monster is very afraid of", supplier_id: 3},
+  {name: "Monopoly", price: "60.0", image_url: "https://m.media-amazon.com/images/I/71931gxE2PL._AC_SX679_.jpg", description: "An educational game about how the rich always win", supplier_id: 1},
+  {name: "Torch", price: "12.0", image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Lewes_Bonfire%2C_discarded_torch.jpg/1024px-Lewes_Bonfire%2C_discarded_torch.jpg", description: "Something frankenstien's monster is very afraid of", supplier_id: 1},
+  {name: "apple", price: "5.0", image_url: "https://upload.wikimedia.org/wikipedia/commons/6/69/Aroma_%28apple%29.jpg", description: "An apple", supplier_id: 1},
+  {name: "Banana", price: "10.0", image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Banana_and_cross_section.jpg/1024px-Banana_and_cross_section.jpg", description: "A banana", supplier_id: 1},
+  {name: "Torch", price: "14.0", image_url: "frankestiens_monsters_fears.jpeg", description: "Something frankenstien's monster is very afraid of", supplier_id: 1},
+  {name: "Taerch", price: "14.0", image_url: "frankestiens_monsters_fears.jpeg", description: "Something frankenstien's monster is very afraid of", supplier_id: 1}
+])
