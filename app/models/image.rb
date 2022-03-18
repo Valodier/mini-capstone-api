1f846ade4023251d725177b93f8d1cf8f1e26e49
William Meyer <williamjmiii@gmail.com>
2022-03-18 12:59:45 -0400

    Added has_many and belongs_to methods in models

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,7 @@
 class Image < ApplicationRecord
+  belongs_to :products
+  #^^^ This is the same thing as product method, belongs_to is singluar = find_by
+  # def product
+  #   Product.find_by(id: supplier_id)
+  # end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,20 @@
 class Product < ApplicationRecord
-  
   # validates :price, numericality: {only_decimal: true}
   # validates :name, length: {minimum: 2}
   # validates :name, presence: true
   # validates :name, uniqueness: true
+  
+  belongs_to :supplier
+  #^^^ this is the same as supplier method, belongs_to is singular = find_by
+  # def supplier
+  #   Supplier.find_by(id: supplier_id)
+  # end
 
-  def supplier
-    Supplier.find_by(id: supplier_id)
-  end
-
-  def images
-    Image.where(product_id: id)
-  end
+  has_many :images
+  #^^^ This is the same as images method, has_many is plural = .where
+  # def images
+  #   Image.where(product_id: id)
+  # end
 
   def is_discounted
     price <= 10
