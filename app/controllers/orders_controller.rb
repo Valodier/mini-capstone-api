17d408c9e2980dcbeeffbe3528a23b2cb215824b
William Meyer <williamjmiii@gmail.com>
2022-03-21 15:10:20 -0400

    added create action to orders controller, added view for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,21 @@
 class OrdersController < ApplicationController
+  
+  def create
+    order = Order.new(
+      user_id: params["user_id"],
+      product_id: params["product_id"],
+      quantity: params["quantity"],
+      subtotal: params["subtotal"],
+      tax: params["tax"],
+      totals: params["totals"]
+    )
+
+    if order.save
+      
+    else
+      render json: {error: @orders.errors.full_messages},
+      status: 422
+    end
+  end
+
 end
