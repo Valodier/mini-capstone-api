dc3e62eeb0487c5cbb22bb96275a750263d16f6d
William Meyer <valodier@icloud.com>
2022-03-24 13:58:22 -0400

    added through table associations, then rewrote with shortcut

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,11 @@
 class Category < ApplicationRecord
   has_many :category_products
-
-  def category_products.map |category_product| do
-      category_product.product 
-    end   
-  end
+  has_many :categories, through: :category_products
+  ### This down here is the same as this line ^^^
+  # def categories
+  #   category_products.map do |category_product|
+  #     category_product.category
+  #   end   
+  # end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ApplicationRecord
   has_many :orders
-  has_many :category_products
   
   belongs_to :supplier
   #^^^ this is the same as supplier method, belongs_to is singular = find_by
@@ -14,16 +13,19 @@ class Product < ApplicationRecord
   #   Image.where(product_id: id)
   # end
 
+  has_many :category_products
+  has_many :products, through: :category_products
+  ###This down here the same as this ^^^
+  def products
+    category_products.map do |category_product|
+      category_product.category 
+    end   
+  end
+  
   # validates :price, numericality: {only_decimal: true}
   # validates :name, length: {minimum: 2}
   # validates :name, presence: true
   # validates :name, uniqueness: true
-
-  def category_products.map |category_product| do
-    category_product.category 
-  end   
-end
-  
 
   def is_discounted
     price <= 10
