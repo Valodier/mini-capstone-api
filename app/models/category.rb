19ff042d6559ebcad1f92491067403d70587c856
William Meyer <valodier@icloud.com>
2022-03-24 13:51:39 -0400

    adds many/belong associations between category/category_products/products

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,9 @@
 class Category < ApplicationRecord
+  has_many :category_products
+
+  def category_products.map |category_product| do
+      category_product.product 
+    end   
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,22 +1,29 @@
 class Product < ApplicationRecord
   has_many :orders
-  
-  # validates :price, numericality: {only_decimal: true}
-  # validates :name, length: {minimum: 2}
-  # validates :name, presence: true
-  # validates :name, uniqueness: true
+  has_many :category_products
   
   belongs_to :supplier
   #^^^ this is the same as supplier method, belongs_to is singular = find_by
   # def supplier
   #   Supplier.find_by(id: supplier_id)
   # end
-
+  
   has_many :images
   #^^^ This is the same as images method, has_many is plural = .where
   # def images
   #   Image.where(product_id: id)
   # end
+
+  # validates :price, numericality: {only_decimal: true}
+  # validates :name, length: {minimum: 2}
+  # validates :name, presence: true
+  # validates :name, uniqueness: true
+
+  def category_products.map |category_product| do
+    category_product.category 
+  end   
+end
+  
 
   def is_discounted
     price <= 10
