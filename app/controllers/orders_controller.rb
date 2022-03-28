bb83f14f6516aee3871d6153ea7024d29e9bbf43
William Meyer <valodier@icloud.com>
2022-03-28 15:34:11 -0400

    moved total maths to order model

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,24 +16,15 @@ class OrdersController < ApplicationController
   def create
     carted_products = current_user.carted_products.where(status:"carted")
 
-    calculated_subtotal = 0 
-    carted_products.each do |carted_product|
-      calculated_subtotal += carted_product.quantity * carted_product.product.price
-    end
-    #product.price * params[:quantity]
-
-    calculated_tax = calculated_subtotal * 0.09
-    calculated_total = calculated_subtotal + calculated_tax
-
     @order = Order.new(
       user_id: current_user.id,
-      subtotal: calculated_subtotal,
-      tax: calculated_tax,
-      total: calculated_total
+      # subtotal: calculated_subtotal,
+      # tax: calculated_tax,
+      # total: calculated_total
     )
-
     if @order.save
       carted_products.update_all(status: "purchased", order_id: @order.id)
+      @order.update_totals
       render :show
     else
       render json: {errors: @order.errors.full_messages},
