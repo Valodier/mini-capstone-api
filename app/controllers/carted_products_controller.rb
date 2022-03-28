e1b0a7fa00db49e8a0cc827cd9a98f88bcf86195
William Meyer <valodier@icloud.com>
2022-03-28 15:16:20 -0400

    Adds views for carted products, populates fields

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,7 +1,7 @@
 class CartedProductsController < ApplicationController
 
   def create
-    carted_product = CartedProduct.new(
+    @carted_product = CartedProduct.new(
       user_id: current_user.id,
       product_id: params["product_id"],
       quantity: params["quantity"],
@@ -9,8 +9,8 @@ class CartedProductsController < ApplicationController
       order_id: params["order_id"]
     )
 
-    if carted_product.save
-      render json: carted_product
+    if @carted_product.save
+      render json: @carted_product
     else
       render json: {errors: carted_product.errors.full_messages},
       status: 422
@@ -18,9 +18,9 @@ class CartedProductsController < ApplicationController
   end
 
   def index
-    carted_products = current_user.carted_products.where(status:"carted")
+    @carted_products = current_user.carted_products.where(status:"carted")
     #carted_products = CartedProduct.find_by(current_user)
-    render json: carted_products
+    render json: @carted_products
   end
 
   def destroy
